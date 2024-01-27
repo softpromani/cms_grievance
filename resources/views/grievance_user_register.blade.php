3c17c41c6bbfe86b37ae8dd1d743fc4b760974e3
unknown <shivam786098jais00@gmail.com>
2024-01-27 13:36:51 +0530

    grievance_form

--- resources/views/grievance_user_register.blade.php
+++ resources/views/grievance_user_register.blade.php
@@ -10,49 +10,88 @@
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4 class="card-title">Multiple Column</h4>
+                                    <h4 class="card-title">Grievance User Register</h4>
                                 </div>
                                 <div class="card-body">
                                     <form class="form">
                                         <div class="row">
-                                            <div class="col-md-6 col-12">
+                                            <div class="col-12">
                                                 <div class="mb-1">
-                                                    <label class="form-label" for="first-name-column">First Name</label>
-                                                    <input type="text" id="first-name-column" class="form-control" placeholder="First Name" name="fname-column" />
+                                                    <label class="form-label" for="first-name-column">Adsmn./Reg./Roll<sup style="color:red;font-size:15px">*</sup></label>
+                                                    {{-- <input type="text" id="first-name-column" class="form-control" placeholder="First Name" name="fname-column" /> --}}
+                                                    <select name="cars" class="form-control" id="cars">
+                                                        <option value="">Adsmn./Reg./Roll</option>
+                                                        <option value="saab">Saab</option>
+                                                        <option value="mercedes">Mercedes</option>
+                                                        <option value="audi">Audi</option>
+                                                    </select>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6 col-12">
+                                            <div class="col-md-4 col-12">
                                                 <div class="mb-1">
-                                                    <label class="form-label" for="last-name-column">Last Name</label>
-                                                    <input type="text" id="last-name-column" class="form-control" placeholder="Last Name" name="lname-column" />
+                                                    <label class="form-label" for="first-name-column">First Name<sup style="color:red;font-size:15px">*</sup></label>
+                                                    <input type="text" id="first-name-column" class="form-control" placeholder="First Name" name="fname" />
                                                 </div>
                                             </div>
-                                            <div class="col-md-6 col-12">
+                                            <div class="col-md-4 col-12">
                                                 <div class="mb-1">
-                                                    <label class="form-label" for="city-column">City</label>
-                                                    <input type="text" id="city-column" class="form-control" placeholder="City" name="city-column" />
+                                                    <label class="form-label" for="last-name-column">Last Name<sup style="color:red;font-size:15px">*</sup></label>
+                                                    <input type="text" id="last-name-column" class="form-control" placeholder="Last Name" name="lname" />
                                                 </div>
                                             </div>
-                                            <div class="col-md-6 col-12">
+                                            <div class="col-md-4 col-12">
                                                 <div class="mb-1">
-                                                    <label class="form-label" for="country-floating">Country</label>
-                                                    <input type="text" id="country-floating" class="form-control" name="country-floating" placeholder="Country" />
+                                                    <label class="form-label" for="last-name-column">Gender<sup style="color:red;font-size:15px">*</sup></label><br/>
+                                                    <input type="radio" id="last-name-column"  name="gender" />Male
+                                                    <input type="radio"  name="gender" /> Female
                                                 </div>
                                             </div>
-                                            <div class="col-md-6 col-12">
+                                            <div class="col-md-4 col-12">
                                                 <div class="mb-1">
-                                                    <label class="form-label" for="company-column">Company</label>
-                                                    <input type="text" id="company-column" class="form-control" name="company-column" placeholder="Company" />
+                                                    <div class="mb-1">
+                                                        <label class="form-label" for="email-id-column">Email<sup style="color:red;font-size:15px">*</sup></label>
+                                                        <input type="email" id="email-id-column" class="form-control" name="email" placeholder="Email" />
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6 col-12">
+                                            <div class="col-md-4 col-12">
                                                 <div class="mb-1">
-                                                    <label class="form-label" for="email-id-column">Email</label>
-                                                    <input type="email" id="email-id-column" class="form-control" name="email-id-column" placeholder="Email" />
+                                                    <label class="form-label" for="country-floating">Mobile<sup style="color:red;font-size:15px">*</sup></label>
+                                                    <input type="text" id="country-floating" class="form-control" name="mobile" placeholder="Mobile" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 col-12">
+                                                <div class="mb-1">
+                                                    <label class="form-label" for="country-floating">Password<sup style="color:red;font-size:15px">*</sup></label>
+                                                    <input type="text" id="country-floating" class="form-control" name="password" placeholder="password" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 col-12">
+                                                <div class="mb-1">
+                                                    <label class="form-label" for="company-column">Confirm Password<sup style="color:red;font-size:15px">*</sup></label>
+                                                    <input type="text" id="company-column" class="form-control" name="confirm_password" placeholder="Confirm Password" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 col-12">
+                                                <div class="mb-1">
+                                                    <label class="form-label" for="email-id-column">Course<sup style="color:red;font-size:15px">*</sup></label>
+                                                    <input type="email" id="email-id-column" class="form-control" name="course" placeholder="Course" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 col-12">
+                                                <div class="mb-1">
+                                                    <label class="form-label" for="email-id-column">Year/Semester</label>
+                                                    <input type="email" id="email-id-column" class="form-control" name="year_semester" placeholder="Year/Semester" />
                                                 </div>
                                             </div>
                                             <div class="col-12">
-                                                <button type="reset" class="btn btn-primary me-1">Submit</button>
+                                                <div class="mb-1">
+                                                    <label class="form-label" for="email-id-column">Course Complete Date</label>
+                                                    <input type="date" id="email-id-column" class="form-control" name="email-id-column" placeholder="Course Complete Date" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <button type="submit" class="btn btn-primary me-1">Submit</button>
                                                 <button type="reset" class="btn btn-outline-secondary">Reset</button>
                                             </div>
                                         </div>
